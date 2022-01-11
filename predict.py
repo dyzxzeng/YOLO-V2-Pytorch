31eed80332a7bdfeb7c52848cabd4eb095121325
dyzxzeng <65577828+dyzxzeng@users.noreply.github.com>
2022-01-11 21:00:12 +0800

    Add files via upload

---
+++ predict.py
@@ -0,0 +1,53 @@
+import time
+
+import cv2
+import numpy as np
+from PIL import Image
+
+from yolo import YOLO
+
+if __name__ == "__main__":
+    yolo = YOLO()
+
+    #   mode：
+    #   'predict' for single picture
+    #   'dir_predict' for all image in \img
+
+    mode = "predict"
+
+
+    #   dir_origin_path points to the root of images
+    #   dir_save_path points to save path
+
+    dir_origin_path = "img/"
+    dir_save_path   = "img_out/"
+
+    if mode == "predict":
+        while True:
+            img = input('Input image filename:')
+            try:
+                image = Image.open(img)
+            except:
+                print('Open Error! Try again!')
+                continue
+            else:
+                r_image = yolo.detect_image(image)
+                r_image.show()
+
+    elif mode == "dir_predict":
+        import os
+
+        from tqdm import tqdm
+
+        img_names = os.listdir(dir_origin_path)
+        for img_name in tqdm(img_names):
+            if img_name.lower().endswith(('.bmp', '.dib', '.png', '.jpg', '.jpeg', '.pbm', '.pgm', '.ppm', '.tif', '.tiff')):
+                image_path  = os.path.join(dir_origin_path, img_name)
+                image       = Image.open(image_path)
+                r_image     = yolo.detect_image(image)
+                if not os.path.exists(dir_save_path):
+                    os.makedirs(dir_save_path)
+                r_image.save(os.path.join(dir_save_path, img_name))
+                
+    else:
+        raise AssertionError("Please specify the correct mode: 'predict', 'video', 'fps' or 'dir_predict'.")
